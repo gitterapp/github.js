238a29b18e2e9bfaac69c17f4e47b8f1a0faa6fc
upcwangying <upcwangying@126.com>
2019-08-10 23:43:58 +0800

    feat: 修改eslint规则

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Github Apis
 
+[github-contributions](packages/github-contributions): A library to get `GitHub contributions` for JavaScript developers.
+
+[github-languages](packages/github-languages): A library to get `GitHub languages` for JavaScript developers.
+
+[github-trending](packages/github-trending): A library to get `GitHub trending repositories` or `GitHub trending developers` for JavaScript developers.
+
+## License
+
+MIT
--- packages/github-languages/index.js
+++ packages/github-languages/index.js
@@ -1,32 +1,35 @@
-const request = require("request-promise");
-const parser = new DOMParser();
+const request = require('request-promise')
+
+const parser = new DOMParser()
 
 class Language {
-    constructor(text) {
-        this.text = text;
-    }
+  constructor(text) {
+    this.text = text
+  }
 
-    toString() {
-        return "(" + this.text + ")";
-    }
+  toString() {
+    return `(${this.text})`
+  }
 }
 
-const getLanguages = () => {
-    return new Promise((resolve, reject) => {
-        request("https://github.com/trending")
-            .then(response => {
-                if (!response) reject("response is null!")
-                const docHtml = parser.parseFromString(response, "text/html");
-                const nodes = docHtml
-                    .querySelector(".select-menu-list")
-                    .querySelectorAll(".select-menu-item-text");
-                const languages = nodes.map(node => Language(node.innerHTML)).toArray();
-                resolve(languages);
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
-};
+const getLanguages = () => new Promise((resolve, reject) => {
+  request('https://github.com/trending')
+    .then(response => {
+      if (!response) {
+        reject('response is null!')
+        return
+      }
+      const docHtml = parser.parseFromString(response, 'text/html')
+      const nodes = docHtml
+        .querySelector('.select-menu-list')
+        .querySelectorAll('.select-menu-item-text')
+      const languages = nodes.map(node => Language(node.innerHTML)).toArray()
+      resolve(languages)
+    })
+    .catch(error => {
+      reject(error)
+    })
+})
 
-export { getLanguages };
+
+export default getLanguages
